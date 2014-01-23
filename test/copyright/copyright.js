dd6bd363f9c7f65e2982cae271994a0ffbf1f5d9
Sergey Berezhnoy <veged@yandex-team.ru>
2014-01-23 17:59:10 +0400

    Add support for @copyright (close #5)

---
+++ test/copyright/copyright.js
@@ -0,0 +1,14 @@
+/**
+ * @module my-module
+ * @copyright Ivan Ivanov
+ */
+
+modules.define('my-module', function(provide) {
+
+provide(/** @exports */{
+    method1 : function() {
+
+    }
+});
+
+});
