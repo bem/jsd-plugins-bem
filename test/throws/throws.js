f8fdf01ae86197af049f68df76571ec6d3c9645f
Anton Krichevskii <mrskadi0@gmail.com>
2018-02-04 15:29:32 +0200

    Add support for throws (#21)

---
+++ test/throws/throws.js
@@ -0,0 +1,17 @@
+/**
+ * @module my-module
+ */
+
+modules.define('my-module', function(provide) {
+
+provide(/** @exports my-module */{
+    /**
+     * Description of method1
+     * @throws {Error} some description
+     */
+    method1 : function() {
+
+    }
+});
+
+});
